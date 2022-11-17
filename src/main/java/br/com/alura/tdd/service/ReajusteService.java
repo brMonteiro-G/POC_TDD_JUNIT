78fad5ab6a61c073390f34148c896875103b38d7
Gabriel Monteiro 60003918 <gabriel.rocha@midway.com.br>
2022-11-17 09:12:35 -0300

    Adicionando strategy pattern ao enum de desempenho

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -9,17 +9,9 @@ public class ReajusteService {
 
 
     public void concederBonificacao(Funcionario funcionario, Desempenho desempenho) {
-        if(desempenho.equals(Desempenho.A_DESEJAR)){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.setSalario(reajuste);
-        }
-        else if(desempenho.equals(Desempenho.BOM)){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.setSalario(reajuste);
-        }
-        else{
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.setSalario(reajuste);
-        }
+
+        BigDecimal reajuste = funcionario.getSalario().multiply(desempenho.percentualReajuste());
+        funcionario.setSalario(reajuste);
+
     }
 }
